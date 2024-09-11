064522359d458cd6d5977380ca0a03202101f282
maria21trm <jkkmt17@gmail.com>
2024-09-11 19:23:21 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+homework_count = 12
+time_spent = 1.5
+course_name = "Python"
+time_for_one_task = 0.125
+print("Курс:",course_name,",","всего задач:",homework_count,",","затрачено часов:",time_spent,",","среднее время выполнения",time_for_one_task,"часа.")
